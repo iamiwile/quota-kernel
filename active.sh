7b9cff5ffc067995dfda26fddbb747807dc7dc12
Michael Williams <iamiwile@msn.com>
2026-05-22 11:31:12 -0500

    Add files via upload

---
+++ active.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+HOME_SOURCE="$(findmnt -n -o SOURCE /home)"
+ROOT_SOURCE="$(findmnt -n -o SOURCE /)"
+HOME_FSTYPE="$(findmnt -n -o FSTYPE /home)"
+
+echo "ROOT : $ROOT_SOURCE"
+echo "HOME : $HOME_SOURCE"
+echo "FS   : $HOME_FSTYPE"
+echo ""
+
+# =====================================================
+# VALIDAR /home
+# =====================================================
+
+if [[ "$HOME_FSTYPE" == "ext4" && "$HOME_SOURCE" != "$ROOT_SOURCE" ]]; then
+    echo "Activando EXT4 kernel quota."
+    sudo umount /home
+    sudo tune2fs -O quota "$HOME_SOURCE"
+    sudo mount /home
+    echo "OK"
+fi
+
+echo ""
+echo "Ejecutando e2fsck..."
+sudo e2fsck -f "$HOME_SOURCE"
+echo ""
+echo "Verificando feature..."
+sudo tune2fs -l "$HOME_SOURCE" | grep quota
+
+echo ""
+echo "==========================================="
+echo " COMPLETADO"
+echo "==========================================="
